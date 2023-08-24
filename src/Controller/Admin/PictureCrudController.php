7e17a330f901fc94f2b15518c2b905e2c12a5648
Maude-MEURY <maude.meury@gmail.com>
2023-08-24 14:24:38 +0200

    "feat"(easyAdmin): add PictureCrudController

---
+++ src/Controller/Admin/PictureCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Picture;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class PictureCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Picture::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new("id", "Id de l'image")->hideOnForm(),
+            TextField::new("name", "Nom de l'image"),
+            TextField::new("picture_legeng","Légende de l'image"),
+            UrlField::new("url","Url de l'image"),
+            
+        ];
+    }
+    
+}
